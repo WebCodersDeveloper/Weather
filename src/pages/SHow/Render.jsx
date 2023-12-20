8deb5558a69b8b0ec0d41a5b95bbfefe0ec7475c
WebCodersDeveloper <ibrohimahmedov869@gmail.com>
2023-12-20 17:39:55 +0500

    edit

--- src/pages/SHow/Render.jsx
+++ src/pages/SHow/Render.jsx
@@ -89,7 +89,7 @@
               </div>
               <div className="info_wh">
                 <div className="wind">
-                  {/* <h1>salom </h1> */}
+                  <h1>salom </h1>
                   <img src="../../../public/compas_icon.png" className="compas_IC" style={{transform: `rotate(${data.current.wind_degree}deg)`}} alt="" />
                   <h3>{data.current.wind_kph} km/h</h3>
                 </div>
